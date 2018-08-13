9f828940e8f1b88b6804908a4576cb90a3849943
coaxial <hi@64b.it>
2018-08-13 09:24:35 -0400

    Move git to subgenerator

--- __tests__/app.js
+++ __tests__/app.js
@@ -1,51 +1,5 @@
 'use strict';
-const gitRemoteOriginUrl = require('git-remote-origin-url');
-const { readFileSync } = require('fs');
-const assert = require('yeoman-assert');
-const helpers = require('yeoman-test');
-const path = require('path');
 
 describe('generator-ansible-play:app', () => {
-  describe('by default', () => {
-    beforeAll(() => {
-      return helpers.run(path.join(__dirname, '../generators/app')).withPrompts({
-        initGit: true,
-        gitHost: 'example.com',
-        gitUser: 'coaxial',
-        gitRepo: 'myrepo',
-      });
-    });
-
-    it('initializes a git repo', () => {
-      assert.file('.git');
-    });
-
-    it('generates a .gitignore file', () => {
-      const actual = readFileSync('.gitignore', 'utf8');
-
-      expect(actual).toMatchSnapshot();
-    });
-
-    it('sets the correct origin URL', () => {
-      const expected = 'git@example.com:coaxial/myrepo.git';
-
-      gitRemoteOriginUrl().then(originUrl => expect(originUrl).toEqual(expected));
-    });
-  });
-
-  describe('when told not to init a git repo', () => {
-    beforeAll(() => {
-      return helpers
-        .run(path.join(__dirname, '../generators/app'))
-        .withPrompts({ initGit: false });
-    });
-
-    it('will not init a git repo', () => {
-      assert.noFile('.git');
-    });
-
-    it('will not generate a .gitignore', () => {
-      assert.noFile('.gitignore');
-    });
-  });
+  it('runs', () => {});
 });
---
+++ generators/git/index.js
@@ -0,0 +1,58 @@
+'use strict';
+const Generator = require('yeoman-generator');
+
+module.exports = class extends Generator {
+  prompting() {
+    const prompts = [
+      {
+        type: 'confirm',
+        name: 'initGit',
+        message: 'Initialize a git repository and generate a .gitignore?',
+        default: true,
+        required: true,
+        store: true,
+      },
+      {
+        type: 'input',
+        name: 'gitHost',
+        message: 'What is the hostname for the server hosting the git repo?',
+        default: 'github.com',
+        when: answers => answers.initGit,
+        required: true,
+        store: true,
+      },
+      {
+        type: 'input',
+        name: 'gitUser',
+        message: 'What is the username on the git server?',
+        when: answers => answers.initGit,
+        required: true,
+        store: true,
+      },
+      {
+        type: 'input',
+        name: 'gitRepo',
+        message: "What is the repo's name on the git server?",
+        when: answers => answers.initGit,
+        required: true,
+      },
+    ];
+
+    return this.prompt(prompts).then(props => {
+      this.props = props;
+    });
+  }
+
+  writing() {
+    const p = this.props;
+
+    if (p.initGit) {
+      const originUrl = `git@${p.gitHost}:${p.gitUser}/${p.gitRepo}.git`;
+      this.spawnCommandSync('git', ['init', '--quiet']);
+      this.fs.copy(this.templatePath('gitignore'), this.destinationPath('.gitignore'));
+      this.spawnCommandSync('git', ['remote', 'add', 'origin', originUrl]);
+    }
+  }
+
+  install() {}
+};

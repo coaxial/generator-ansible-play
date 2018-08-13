242bd90c78a8d87c5c6e3e4ecd09720021ea63e8
Pierre <coaxial@users.noreply.github.com>
2018-08-13 18:02:33 +0000

    Initialize empty git repo (#1)
    * Initialize a git repo
    
    * Lint
    
    * Format
    
    * Handle git repo init
    
    * Lint
    
    * Update packages
    
    * Make git answers mandatory
    
    * Remove unnecessary step
    
    * Move git to subgenerator
    
    * Remove Node v4 target
    
    * Configure importjs
    
    * Fix test

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,38 +1,12 @@
 'use strict';
 const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
 
 module.exports = class extends Generator {
   prompting() {
-    // Have Yeoman greet the user.
-    this.log(
-      yosay(`Welcome to the kryptonian ${chalk.red('generator-ansible-play')} generator!`)
-    );
-
-    const prompts = [
-      {
-        type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
-        default: true
-      }
-    ];
-
-    return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
-    });
+    this.composeWith(require.resolve('../git'));
   }
 
-  writing() {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
-  }
+  writing() {}
 
-  install() {
-    this.installDependencies();
-  }
+  install() {}
 };

35304511cc85f5c0f3d871194c3966533a387e2c
coaxial <hi@64b.it>
2018-08-13 22:06:36 -0400

    Create skel and Makefile

---
+++ __tests__/playbook.js
@@ -0,0 +1,23 @@
+'use strict';
+const { readFileSync } = require('fs');
+const assert = require('yeoman-assert');
+const helpers = require('yeoman-test');
+const path = require('path');
+
+describe('generator-ansible-play:playbook', () => {
+  beforeAll(() => {
+    return helpers.run(path.join(__dirname, '../generators/playbook')).withPrompts({
+      limit: 'test-host',
+    });
+  });
+
+  it('creates the skeleton', () => {
+    assert.file(['handlers', 'tasks', 'templates', 'vars']);
+  });
+
+  it('creates the Makefile', () => {
+    const actual = readFileSync('Makefile', 'utf8');
+
+    expect(actual).toMatchSnapshot();
+  });
+});
---
+++ generators/playbook/index.js
@@ -0,0 +1,39 @@
+'use strict';
+const Generator = require('yeoman-generator');
+const mkdirp = require('mkdirp');
+
+module.exports = class extends Generator {
+  prompting() {
+    const prompts = [
+      {
+        name: 'limit',
+        message:
+          'Which host will this playbook target? (single host or comma separated list, eg `host1,host2`)',
+        required: true,
+        default: 'all',
+      },
+    ];
+
+    return this.prompt(prompts).then(props => {
+      this.props = props;
+    });
+  }
+
+  writing() {
+    const p = this.props;
+    const createSkel = () => {
+      const dirs = ['handlers', 'tasks', 'templates', 'vars'];
+      dirs.forEach(dir => mkdirp(dir));
+    };
+    const createFiles = () => {
+      this.fs.copyTpl(this.templatePath('Makefile'), this.destinationPath('Makefile'), {
+        limit: p.limit,
+      });
+    };
+
+    createSkel();
+    createFiles();
+  }
+
+  install() {}
+};
